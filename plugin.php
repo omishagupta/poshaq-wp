d1d991c9dfabe49ec8e59271842c3ae31b2f81d2
Nitesh Oswal <nit.oswal@gmail.com>
2017-05-28 18:42:40 +0530

    Just the wraps

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       poshaQ
+ * Plugin URI:        http://getposhaq.com/
+ * Description:       We add intelligence to your blogs.
+ * Version:           1.0.0
+ * Author:            poshaQ
+ * Author URI:        http://getposhaq.com/
+ * License:           MIT
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
